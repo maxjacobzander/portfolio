f6be9c7ab0b0265906ff2889e0c075e97a8f9aca
maxjacobzander <maxjacobzander@gmail.com>
2021-02-12 23:37:10 -0600

    Fixed Router, Added Resume pdf, created intial landing and resume components

---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Max Zander</h1>
+                <h1>Full-Stack Web Developer</h1>
+            </div>
+        );
+    }
+}
+
+export default Landing;
---
+++ src/components/Resume.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+// import {connect} from 'react-redux'
+// import { Document, Page } from 'react-pdf';
+// import {Max_Zander_SE_Resume_Feb_2021} from  '../documents/Max_Zander_SE_Resume_Feb_2021.pdf'
+
+class Resume extends Component {
+
+    render() {
+        return (
+            <div>
+                {/* <Document file={Max_Zander_SE_Resume_Feb_2021}>
+                    <Page pageNumber={1} />
+                </Document> */}
+            </div>
+        );
+    }
+}
+
+export default Resume;
