13c971d1dd9ab32aaf7cfda529e6017384596b89
maxjacobzander <maxjacobzander@gmail.com>
2021-02-18 11:30:08 -0600

    Created Projects component and imported css for panels

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './components/About';
 import Resume from './components/Resume';
 import Home from './containers/Home';
+import Projects from './components/Projects';
 // import './App.css';
 import './style.css';
 
@@ -17,6 +18,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
             <Route exact path="/resume" component={Resume} />
+            <Route exact path="/projects" component={Projects} />
           </Switch>
         </div>
       </Router>
---
+++ src/components/Projects.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import LiquorCabinet from '../documents/Liquor_Cabinet.jpg';
+import EarTrainer from '../documents/Ear_Trainer.jpg';
+import Bucket from '../documents/Bucket.jpg';
+import BravoScoreLibrary from '../documents/Bravo_Score_Library.jpg'
+
+class Projects extends Component {
+
+    panels = document.querySelectorAll('.panel')
+
+    render() {
+        return (
+            <div class="project-container">
+                <div className="panel active" style={{backgroundImage: `url(${LiquorCabinet})`}}>
+                    <h3>Liquor Cabinet (React/Redux Front-End, Rails API Back-End)</h3>
+                </div>
+                <div className="panel" style={{backgroundImage: `url(${EarTrainer})`}}>
+                    <h3>Ear Trainer (Vanilla JavaScript Front-End, Rails API Back-End)</h3>
+                </div>
+                <div className="panel" style={{backgroundImage: `url(${Bucket})`}}>
+                    <h3>Bucket (Rails Application)</h3>
+                </div>
+                <div className="panel" style={{backgroundImage: `url(${BravoScoreLibrary})`}}>
+                    <h3>Bravo Personal Score Library Catalogue (Sinatra Application)</h3>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Projects;
