60e04643b21caaf74de6b50c49c7430036470277
maxjacobzander <maxjacobzander@gmail.com>
2021-02-17 19:48:39 -0600

    Changed resume component to embed tag

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,15 +1,30 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-class Resume extends Component {
+// class Resume extends Component {
 
-    render() {
-        return (
-            <div>
-                <object className="resume" data="https://f675d2b8-bbcd-4b2f-b6a0-eafac903cb04.filesusr.com/ugd/b2c140_d9bc5b6a78d84f189e5a15e839fab2b5.pdf" type="application/pdf" width="100%" height="100%">
-                </object>
-            </div>
-        );
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <object className="resume" data="https://f675d2b8-bbcd-4b2f-b6a0-eafac903cb04.filesusr.com/ugd/b2c140_d9bc5b6a78d84f189e5a15e839fab2b5.pdf" type="application/pdf">
+//                 </object>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Resume;
+
+
+import React from "react";
+import PDF from "../documents/Max_Zander_SE_Resume_Feb_2021.pdf";
+
+const Resume = () => (
+  <embed
+    src={PDF + "#toolbar=0"}
+    type="application/pdf"
+    height={645}
+    width={500}
+  />
+);
 
 export default Resume;
